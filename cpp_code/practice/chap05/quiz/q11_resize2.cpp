d74f21323c80f442e81ad5507300cf31955cfdc8
Gwiin <injoa4526@gmail.com>
2026-04-16 17:16:35 +0900

    update

--- cpp_code/practice/chap05/quiz/q11_resize2.cpp
+++ cpp_code/practice/chap05/quiz/q11_resize2.cpp
@@ -12,14 +12,17 @@ public:
     MyStack();
     MyStack(int size);
     MyStack(const MyStack &src);
-    MyStack(const MyStack &src, int size);
+
     ~MyStack();
+
+    void resize(int size);
 
     bool push(int n);
     bool pop(int &n);
+    
     int getSize() { return size; }
     int getTos() { return tos;}
-    int show(int index) { return p[index];}
+    void show();
 };
 
 MyStack::MyStack(){
@@ -42,29 +45,36 @@ MyStack::MyStack(const MyStack &src){
     }
 }
 
-MyStack::MyStack(const MyStack &src, int size){
-    if(src.tos > size){
-        cout << "채워진 스택사이즈보다 작아서 손실이 생깁니다" << endl;
-    }
-    else{
-        this->size = size;
-        this->tos = src.tos;
-        this->p = new int [this->size];
-        for(int i = 0 ; i < this->tos ; i++){
-            this->p[i] = src.p[i];
-        }
-        for(int i = this->tos ; i < size ; i++){
-            this->p[i] = 0;
-        }
-    }
-}
-
 
 MyStack::~MyStack(){
     if(p){
         cout << "할당 해제" << endl;
         delete [] p;
     }
+}
+
+
+void MyStack::resize(int size){
+    int *tmp = new int [size];
+    cout << "새로운 확장 공간 할당" << endl;
+    this->size = size;
+    for (int i = 0; i < this->tos ; i++){
+        tmp[i] = this->p[i];
+    }
+    for(int i = this->tos ; i < this->size ; i++){
+        tmp[i] = 0;
+    }
+    delete [] p;
+    cout << "이전 공간 해제" << endl;
+    this->p = tmp;
+}
+
+
+void MyStack::show(){
+    for(int i = 0 ; i < this->size ; i++){
+        cout << this->p[i] << " ";
+    }
+    cout << endl;
 }
 
 
@@ -104,14 +114,11 @@ int main(void){
     b.pop(n);
     cout << "스택 b에서 팝한 값 " << n << endl;
 
-    MyStack c(a,20);
-    cout << c.getSize() << " " << c.getTos() << endl;
-    for (int i = c.getTos() ; i < c.getSize(); i++){
-        c.push(i);
+    a.resize(20);
+    for(int i = a.getTos() ; i < a.getSize() ; i++){
+        a.push(i);
     }
+    a.show();
 
-    for (int i = 0 ; i < c.getSize() ; i++){
-        cout << c.show(i) << " ";
-    }
-    cout << endl;
+    return 0;
 }
